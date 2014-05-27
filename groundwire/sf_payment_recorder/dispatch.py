2e7d90dab1688dc054d0215110f6e9c9558f73e8
Jesse Snyder <jesse@rasikaconsulting.com>
2014-05-27 12:30:31 -0700

    conditional import to handle change of hurry.workflow to getpaid.hurry.workflow

--- groundwire/sf_payment_recorder/dispatch.py
+++ groundwire/sf_payment_recorder/dispatch.py
@@ -1,10 +1,15 @@
 from zope.component import adapter
 from getpaid.core.interfaces import workflow_states
 from getpaid.core.interfaces import IOrder
-from hurry.workflow.interfaces import IWorkflowTransitionEvent
-
 from groundwire.sf_payment_recorder.config import get_settings
 from groundwire.sf_payment_recorder.record import record_order
+
+try:
+    from getpaid.hurry.workflow.interfaces import IWorkflowTransitionEvent
+except ImportError:
+    # BBB for hurry.workflow (pre-namespance change)
+    from hurry.workflow.interfaces import IWorkflowTransitionEvent
+
 
 canceled_states = (workflow_states.order.finance.PAYMENT_DECLINED,
                    workflow_states.order.finance.CANCELLED,
